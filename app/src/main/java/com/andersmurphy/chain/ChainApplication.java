4462327da5849f6ac7c4a41e290d84dc6f016b21
Anders <anders.murphy@kotikan.com>
2016-03-20 14:46:58 +0000

    Enables multidex.

--- app/src/main/java/com/andersmurphy/chain/ChainApplication.java
+++ app/src/main/java/com/andersmurphy/chain/ChainApplication.java
@@ -1,7 +1,13 @@
 package com.andersmurphy.chain;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
 public class ChainApplication extends Application {
-
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
 }
