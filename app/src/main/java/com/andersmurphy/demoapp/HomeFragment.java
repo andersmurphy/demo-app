305b9aa88d137e0b7e13b45d62aa73b937d16d9a
Anders <anders.murphy@kotikan.com>
2016-10-07 21:42:56 +0100

    Adds ButterKnife.

--- app/src/main/java/com/andersmurphy/demoapp/HomeFragment.java
+++ app/src/main/java/com/andersmurphy/demoapp/HomeFragment.java
@@ -5,13 +5,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.HomeFragmentPresenter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class HomeFragment extends Fragment {
+
+    @BindView(R.id.text)
+    TextView textView;
 
     private HomeFragmentPresenter presenter;
 
@@ -21,9 +28,11 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, view);
+        textView.setText("ButterKnife Works!");
 
         presenter = new HomeFragmentPresenter();
-
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
     }
 }
