3056ad3d350d6da3092db25e8fadba117ca23e07
Anders <anders.murphy@kotikan.com>
2016-10-08 20:46:23 +0100

    Renaming your main activity safely with activity alias.

--- app/src/main/java/com/andersmurphy/demoapp/NewMainActivity.java
+++ app/src/main/java/com/andersmurphy/demoapp/NewMainActivity.java
@@ -8,7 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class NewMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
